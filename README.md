08f546dff13270639666a0a9865da3ffa770ed43
dcontreras <dcontreras@indra.es>
2026-06-30 13:39:48 +0200

    Created extension

---
+++ README.md
@@ -0,0 +1,3 @@
+# breakpointer README
+
+Copies the name of the file + the line the cursor is to paste it into a debugger
---
+++ src/extension.ts
@@ -0,0 +1,45 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import path from "path";
+import * as vscode from "vscode";
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log(
+        'Congratulations, your extension "breakpointer" is now active!',
+    );
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    const disposable = vscode.commands.registerCommand(
+        "breakpointer.copyCurrentLine",
+        async () => {
+            const editor = vscode.window.activeTextEditor;
+
+            if (!editor) {
+                return;
+            }
+
+            const document = editor.document;
+            const position = editor.selection.active;
+
+            const file = path.basename(document.fileName)
+
+            const text = `${file}:${position.line}`;
+            
+
+            await vscode.env.clipboard.writeText(text);
+
+            vscode.window.showInformationMessage("Copied line");
+        },
+    );
+
+    context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
